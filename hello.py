f2e3d963b11379b4ab31d54d6a8b3619372020b0
Kevin <nijialiang17@gmail.com>
2026-04-21 13:50:48 +0930

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+Python 3.13.13 (v3.13.13:01104ce1beb, Apr  7 2026, 14:43:30) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> a = Hello World
+SyntaxError: invalid syntax
+>>> a ="Hello world"
+>>> print(a)
+Hello world
