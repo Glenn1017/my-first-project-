531dc2e528014fc4d74f081e1b0c732f899ec2ad
Kevin <nijialiang17@gmail.com>
2026-04-30 10:31:43 +0930

    Add files via upload

---
+++ list&dict.py
@@ -0,0 +1,10 @@
+students = []
+name = input("what's your name?")
+grade = input("what's your grade")
+students.append([name,grade])
+
+student2 = {}
+names = input("what's you name?")
+grades = input("what's you grade")
+student2["name"] = name
+student2["grade"] = grade 
